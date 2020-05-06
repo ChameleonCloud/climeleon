e632a3b5e5dbffb1bc88650f2dcea47be75d5a32
Jason Anderson <jasonanderson@uchicago.edu>
2020-05-06 12:33:11 -0500

    Add network-public-ip-status command

--- cc-openstack/python-climeleon/climeleon/network.py
+++ cc-openstack/python-climeleon/climeleon/network.py
@@ -1,3 +1,4 @@
+import ipaddress
 from operator import itemgetter
 
 from climeleon.base import BaseCommand
@@ -104,4 +105,92 @@ class NetworkSegmentStatusCommand(BaseCommand):
 
         widths = [max(map(len, col)) for col in zip(*rows)]
         for row in rows:
-            print "  ".join((val.ljust(width) for val, width in zip(row, widths)))
+            cols = (val.ljust(width) for val, width in zip(row, widths))
+            print("  ".join(cols))
+
+
+class NetworkPublicIPStatusCommand(BaseCommand):
+    description = """
+    Display all public IP addresses in the 'public' network DHCP range, and
+    their current allocation status (as Floating IP or router gateway), if any.
+    """
+
+    def _public_allocation_pools(self, neutron):
+        networks = neutron.list_networks(name="public").get("networks")
+        if not networks:
+            raise ValueError("Could not find public network")
+
+        public_net = networks[0]
+        subnets = public_net.get("subnets")
+        if not subnets:
+            raise ValueError("No subnets defined on public network")
+
+        allocation_pools = []
+        for subnet_id in subnets:
+            subnet = neutron.show_subnet(subnet_id).get("subnet")
+            allocation_pools.extend(subnet.get("allocation_pools"))
+
+        return allocation_pools
+
+    def run(self):
+        neutron = self.neutron()
+        routers = {}
+
+        ports = neutron.list_ports().get("ports")
+
+        public_ports = [
+            p for p in ports
+            if p["device_owner"] in [
+                "network:floatingip",
+                "network:router_gateway",
+            ]
+        ]
+        ports_by_ip = {
+            p.get("fixed_ips")[0].get("ip_address"): p
+            for p in public_ports
+        }
+
+        allocation_pools = self._public_allocation_pools(neutron)
+        all_addresses = []
+        for p in allocation_pools:
+            start_ip = ipaddress.IPv4Address(p.get("start"))
+            end_ip = ipaddress.IPv4Address(p.get("end"))
+            for net in ipaddress.summarize_address_range(start_ip, end_ip):
+                all_addresses.extend(list(ipaddress.IPv4Network(net)))
+
+        rows = []
+        rows.append([
+            "public_ip",
+            "allocation_type",
+            "project_id",
+        ])
+
+        for public_ip in sorted(all_addresses):
+            port = ports_by_ip.get(str(public_ip))
+
+            allocation_type = "unallocated"
+            project_id = "none"
+
+            if port:
+                device_owner = port.get("device_owner")
+                if device_owner == "network:router_gateway":
+                    allocation_type = "gateway"
+                    router_id = port.get("device_id")
+                    if router_id not in routers:
+                        routers[router_id] = (
+                            neutron.show_router(router_id).get("router"))
+                    project_id = routers[router_id].get("project_id")
+                else:
+                    allocation_type = "floating_ip"
+                    project_id = port.get("project_id")
+
+            rows.append([
+                str(public_ip),
+                allocation_type,
+                project_id,
+            ])
+
+        widths = [max(map(len, col)) for col in zip(*rows)]
+        for row in rows:
+            cols = (val.ljust(width) for val, width in zip(row, widths))
+            print("  ".join(cols))

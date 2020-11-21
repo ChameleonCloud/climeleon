b26c86320076b6e6cc3504996a53b28309360380
Jason Anderson <jasonanderson@uchicago.edu>
2020-11-20 19:34:32 -0600

    Add node enrollment script

--- cc-openstack/python-climeleon/climeleon/base.py
+++ cc-openstack/python-climeleon/climeleon/base.py
@@ -3,20 +3,10 @@ import operator
 import re
 import sys
 
-from blazarclient.client import Client as BlazarClient
-from glanceclient.client import Client as GlanceClient
-from gnocchiclient.v1.client import Client as GnocchiClient
-from ironicclient import client as IronicClient
-from keystoneauth1 import adapter, loading, session
-from keystoneauth1.identity import v3
-from keystoneclient.v3.client import Client as KeystoneClient
-from neutronclient.v2_0.client import Client as NeutronClient
-from novaclient.client import Client as NovaClient
+import chi
 
 import logging
 logging.basicConfig(level=logging.INFO)
-
-IRONIC_CLIENT_VERSION = 1
 
 
 class BaseCommand:
@@ -34,65 +24,33 @@ class BaseCommand:
 
     def __init__(self, argv=None):
         self.args = self.parse_args(argv)
-
-        self.auth = loading.cli.load_from_argparse_arguments(self.args)
-        self.session = loading.session.load_from_argparse_arguments(
-            self.args, auth=self.auth)
-        # Wrap session in adapter that sets interface/region
-        self.session = adapter.Adapter(
-            session=self.session, interface=self.args.os_interface,
-            region_name=self.args.os_region_name)
-
+        self.session = chi.session()
         self.log = logging.getLogger(__name__)
 
-
     def blazar(self):
-        return BlazarClient("1",
-            service_type="reservation",
-            session=self.session)
+        return chi.blazar(session=self.session)
 
     def glance(self):
-        return GlanceClient("2", session=self.session)
+        return chi.glance(session=self.session)
 
     def gnocchi(self):
-        session_options = dict(auth=self.session.session.auth)
-        adapter_options = dict(interface=self.session.interface,
-                               region_name=self.session.region_name)
-        return GnocchiClient(
-            adapter_options=adapter_options, session_options=session_options
-        )
+        return chi.gnocchi(session=self.session)
 
     def ironic(self):
-        return IronicClient.get_client(
-            IRONIC_CLIENT_VERSION,
-            session=self.session,
-            region_name=self.session.region_name,
-            # Ironic client defaults to 1.9 currently,
-            # "latest" will be latest the API supports
-            os_ironic_api_version='latest'
-        )
+        return chi.ironic(session=self.session)
 
     def keystone(self):
-        return KeystoneClient(
-            session=self.session,
-            interface=self.session.interface,
-            region_name=self.session.region_name
-        )
+        return chi.keystone(session=self.session)
 
     def neutron(self):
-        return NeutronClient(session=self.session)
+        return chi.neutron(session=self.session)
 
     def nova(self):
-        return NovaClient("2", session=self.session)
+        return chi.nova(session=self.session)
 
     def parse_args(self, argv):
         argv = sys.argv[1:]
-
         parser = argparse.ArgumentParser(description=self.description)
-        loading.cli.register_argparse_arguments(
-            parser, argv, default='password')
-        loading.session.register_argparse_arguments(parser)
-        loading.adapter.register_argparse_arguments(parser)
 
         self.register_args(parser)
 
--- cc-openstack/python-climeleon/climeleon/node.py
+++ cc-openstack/python-climeleon/climeleon/node.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python
-"""
-node-assign-switch-ids
-
-Intended as a migration script for https://collab.tacc.utexas.edu/issues/17386, but could
-be useful later. Exists mostly as documentation and perhaps example code that can be re-used.
-"""
 from argparse import FileType
+from blazarclient.exception import BlazarClientException
+import configparser
+from collections import defaultdict
 from dracclient.client import DRACClient
-import operator
+from ironicclient.exc import HTTPNotFound
 import re
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -19,7 +15,11 @@ IRONIC_CLIENT_VERSION = 1
 
 
 class NodeAssignSwitchIDsCommand(BaseCommand):
-
+    """
+    Intended as a migration script for
+    https://collab.tacc.utexas.edu/issues/17386, but could be useful later.
+    Exists mostly as documentation and perhaps example code that can be re-used.
+    """
     @staticmethod
     def uc_assignment_strategy(cls, node, port):
         # Names are like c01, nc24.
@@ -224,3 +224,166 @@ class NodeRotateIPMIPasswordCommand(BaseCommand):
                 if not node.maintenance:
                     ironic.node.set_maintenance(node_id, False)
                     self.log.info("  Reverted node maintenance state")
+
+
+class NodeEnrollCommand(BaseCommand):
+    """
+    Enroll nodes in Ironic/Blazar from a configuration file. The configuration
+    file declares nodes by name and includes important configuration, namely the
+    IPMI authentication details and the physical switch information and NIC MAC
+    addresses for the bare metal node.
+
+    Reads a node configuration from the first positional argument, and defaults
+    to looking for a file "nodes.conf".
+
+    Sample configuration format::
+
+      [node01]
+      # Give a name to the node class; Chameleon uses node class prefixes
+      # followed by specifiers, e.g. compute_haswell or gpu_rtx
+      node_type = compute_haswell
+      ipmi_username = root
+      ipmi_password = hopefully_not_default
+      ipmi_address = 10.10.10.1
+      # Optional, defaults to this value.
+      ipmi_port = 623
+      # Arbitrary terminal port; this is used to plumb a socat process to allow
+      # reading and writing to a virtual console. It is just important that it does
+      # not conflict with another node or host process.
+      ipmi_terminal_port = 30133
+      # Each NIC that should be manageable has its own section. The name of
+      # the section does not matter so long as the first part is the name of a
+      # node defined elsewhere in the config. This example uses the consistent
+      # network device name of the interface.
+      [node01.eno1]
+      switch_name = LeafSwitch01
+      switch_port_id = Te 1/10/1
+      mac_address = 00:00:de:ad:be:ef
+
+    """
+
+    DEFAULT_PROPERTIES = {
+        'capabilities': 'boot_option:local',
+    }
+
+    def register_args(self, parser):
+        parser.add_argument("nodes", metavar="NODE", nargs="*")
+        parser.add_argument("--node-conf", type=FileType("r"),
+                            required=True)
+
+    def run(self):
+        config = configparser.ConfigParser()
+        with self.args.node_conf as f:
+            config.read_file(f)
+
+        node_configs = defaultdict(lambda: dict(
+            driver='ipmi',
+            driver_info={},
+            network_interface='neutron',
+            resource_class='baremetal',
+            properties=self.DEFAULT_PROPERTIES.copy(),
+            ports=[]
+        ))
+        for section in config.sections():
+            if '.' not in section:
+                # Top-level node options
+                node_configs[section]['name'] = section
+                for key, value in config[section].items():
+                    bucket = 'driver_info' if key.startswith('ipmi') else 'properties'
+                    node_configs[section][bucket][key] = value
+            else:
+                # Assume these are port declarations
+                node, _ = section.split('.')
+                node_configs[node]['ports'].append(config[section])
+
+        # Allow user to filter list
+        if self.args.nodes:
+            node_configs = {
+                node: conf for node, conf in node_configs
+                if node in self.args.nodes
+            }
+
+        # Pre-fetch the list of existing Blazar hosts; there is no great way
+        # to look up by any reasonable ID or name.
+        blazar_hosts = self.blazar().host.list()
+        for node, conf in node_configs.items():
+            try:
+                self.enroll_node(conf, blazar_hosts)
+            except:
+                self.log.exception(f'Failed to enroll node {node}!')
+
+    def _to_ironic_patch(self, node_conf):
+        def to_patch(obj, path=''):
+            patch = []
+            for k, v in obj.items():
+                if isinstance(v, dict):
+                    patch.extend(to_patch(v, f'{path}/{k}'))
+                else:
+                    patch.append({
+                        'op': 'add',
+                        'path': f'{path}/{k}',
+                        'value': v
+                    })
+            return patch
+        return to_patch(self._to_ironic_configurables(node_conf))
+
+    def _to_ironic_configurables(self, node_conf):
+        node_configurables = node_conf.copy()
+        # Ports are configured via a separate call
+        node_configurables.pop('ports', None)
+        return node_configurables
+
+    def _ensure_ironic_node(self, ironic, node_conf):
+        node_name = node_conf['name']
+        try:
+            node = ironic.node.get(node_name)
+            if node.provision_state != 'manageable':
+                ironic.node.set_provision_state(node.uuid, 'manage')
+                ironic.node.wait_for_provision_state(node.uuid, 'manageable')
+            patch = self._to_ironic_patch(node_conf)
+            self.log.debug(patch)
+            ironic.node.update(node.uuid, patch)
+            self.log.info(f'Update Ironic node {node.uuid} ({node_name})')
+        except HTTPNotFound:
+            node_create = self._to_ironic_configurables(node_conf)
+            node_create['name'] = node_name
+            self.log.debug(node_create)
+            node = ironic.node.create(node_create)
+            self.log.info(f'Created Ironic node {node.uuid} ({node_name})')
+        return node
+
+    def _ensure_ironic_port(self, ironic, node_port):
+        pass
+
+    def _ensure_blazar_host(self, blazar, ironic_node, blazar_hosts):
+        node_uuid = ironic_node.uuid
+        host_properties = {
+            'node_type': ironic_node.properties['node_type'],
+            'node_name': ironic_node.name,
+            'uid': node_uuid,
+        }
+        host = next(iter([
+            h for h in blazar_hosts
+            if h['hypervisor_hostname'] == node_uuid
+        ]), None)
+        if host:
+            host = blazar.host.update(host['id'], host_properties)
+            self.log.info(f'Updated Blazar host {host["id"]}')
+        else:
+            host = blazar.host.create(node_uuid, **host_properties)
+            self.log.info(f'Created Blazar host {host["id"]}')
+        return host
+
+    def enroll_node(self, node_conf, blazar_hosts):
+        ironic = self.ironic()
+        blazar = self.blazar()
+
+        node = self._ensure_ironic_node(ironic, node_conf)
+
+        for node_port in node_conf['ports']:
+            self._ensure_ironic_port(ironic, node_port)
+
+        ironic.node.set_provision_state(node.uuid, 'provide')
+        ironic.node.wait_for_provision_state(node.uuid, 'available')
+
+        self._ensure_blazar_host(blazar, node, blazar_hosts)

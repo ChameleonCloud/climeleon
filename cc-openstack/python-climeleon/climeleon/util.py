0e1794b8f865a1fea0a30fdfcc6de91ae449c938
Jason Anderson <jasonanderson@uchicago.edu>
2020-12-24 11:15:49 -0600

    Remove python-climeleon in favor of python-chi-operator

--- cc-openstack/python-climeleon/climeleon/util.py
+++
@@ -1,6 +0,0 @@
-def column_align(rows):
-    widths = [max(map(len, col)) for col in zip(*rows)]
-    return [
-        "  ".join(val.ljust(width) for val, width in zip(row, widths))
-        for row in rows
-    ]

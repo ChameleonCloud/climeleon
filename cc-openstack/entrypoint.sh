51e63465e1072bff0ac360b4406478c5e0b79d9e
Adam Cooper <adamcooper@uchicago.edu>
2021-08-04 13:35:18 -0400

    Add config merging script
    The cc-openstack container now takes a dependency on pyyaml.
    
    This patch now is able to take in multiple configs at run-time and merge them into 1. This way, users can simply set the environment variable and no additional installation steps are required.
    
    The cloud-config-merge script is a python script which is copied into the cc-openstack image.
    
    Configs are detected by the cc script, mounted as volumes to the cc-openstack container in the /etc/openstack/clouds.d directory, and then the entrypoint script runs cloud-config-merge with all of the configs as arguments.
    
    cloud-config-merge writes the merged config to /etc/openstack/clouds[-public].yaml

--- cc-openstack/entrypoint.sh
+++ cc-openstack/entrypoint.sh
@@ -2,6 +2,10 @@
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 source /var/lib/cc/venv/bin/activate
 
+if [ -d /etc/openstack/clouds.d/ ]; then
+  cloud-config-merge $(echo /etc/openstack/clouds.d/clouds*.yaml)
+fi
+
 # Ensure important OS_ vars are set as a convenience
 # for older OpenStack CLI interfaces
 set -a
